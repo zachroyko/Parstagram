28b2ec24c56a375e917f66da06e2858614019473
Zach <zach@zachs-mbp.local>
2019-02-17 15:53:38 -0500

    Initial Commit

---
+++ ParstagramTests/ParstagramTests.swift
@@ -0,0 +1,34 @@
+//
+//  ParstagramTests.swift
+//  ParstagramTests
+//
+//  Created by Zach on 2/17/19.
+//  Copyright © 2019 Zach Royko. All rights reserved.
+//
+
+import XCTest
+@testable import Parstagram
+
+class ParstagramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
